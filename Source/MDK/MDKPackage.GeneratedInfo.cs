f2b1d7635244fa5efb2ceed92700cbf2b46bab43
Malware <lyrstad@creventive.net>
2017-10-25 16:41:49 +0200

    Version increase

--- Source/MDK/MDKPackage.GeneratedInfo.cs
+++ Source/MDK/MDKPackage.GeneratedInfo.cs
@@ -8,7 +8,7 @@ namespace MDK
 	    /// <summary>
 		/// The current package version
 		/// </summary>
-		public static readonly Version Version = new Version("1.0.5");
+		public static readonly Version Version = new Version("1.0.6");
 
 	    /// <summary>
 		/// Determines whether this version is a prerelease version
--- Source/MDKServices/ProjectScriptInfo.GeneratedInfo.cs
+++ Source/MDKServices/ProjectScriptInfo.GeneratedInfo.cs
@@ -7,6 +7,6 @@ namespace Malware.MDKServices
 	    /// <summary>
 		/// The current package version this utility assembly targets
 		/// </summary>
-		public static readonly Version TargetPackageVersion = new Version("1.0.5");
+		public static readonly Version TargetPackageVersion = new Version("1.0.6");
 	}
 }
