f991195b0784bfe2d6e06bb8a6616e88bcc1ee42
Malware <lyrstad@creventive.net>
2017-11-17 17:37:46 +0100

    Version kludgery fix

--- Source/MDK/MDKPackage.GeneratedInfo.cs
+++ Source/MDK/MDKPackage.GeneratedInfo.cs
@@ -8,7 +8,7 @@ namespace MDK
 	    /// <summary>
 		/// The current package version
 		/// </summary>
-		public static readonly Version Version = new Version("1.0.7");
+		public static readonly Version Version = new Version("1.0.10");
 
 	    /// <summary>
 		/// Determines whether this version is a prerelease version
