805819e8c69dc1bf05255af8b778ab3f7296d256
Malware <lyrstad@creventive.net>
2018-08-19 10:40:16 +0200

    Visual Studio 2017 15.8 compatibility patch

--- Source/MDK/MDKPackage.GeneratedInfo.cs
+++ Source/MDK/MDKPackage.GeneratedInfo.cs
@@ -8,7 +8,7 @@ namespace MDK
 	    /// <summary>
 		/// The current package version
 		/// </summary>
-		public static readonly Version Version = new Version("1.1.18");
+		public static readonly Version Version = new Version("1.1.25");
 
         /// <summary>
 		/// The required IDE version
