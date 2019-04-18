03aa2dbd7553dbee9d137d039d0444deb772345b
Malware <lyrstad@creventive.net>
2019-04-18 13:33:19 +0200

    Publish

--- Source/MDK/MDKPackage.GeneratedInfo.cs
+++ Source/MDK/MDKPackage.GeneratedInfo.cs
@@ -8,7 +8,7 @@ namespace MDK
 	    /// <summary>
 		/// The current package version
 		/// </summary>
-		public static readonly Version Version = new Version("1.2.1");
+		public static readonly Version Version = new Version("1.2.2");
 
         /// <summary>
 		/// The required IDE version
